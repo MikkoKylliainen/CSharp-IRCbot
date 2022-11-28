8657d5cabcd0361e0fa1dab8cb3e01c96517d56e
Mikko Kylliäinen <mikko.kylliainen@gmail.com>
2022-11-28 21:42:23 +0200

    x

---
+++ Program.cs
@@ -0,0 +1,145 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using System.Threading;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace SnookerBot
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            var ircBot = new IRCbot(
+                server: "irc.quakenet.org",
+                port: 6667,
+                user: "USER SnookerBot 0 * :SnookerBot",
+                nick: "JuddTrump",
+                channel: "#snooker"
+            );
+
+            ircBot.Start();
+        }
+
+        public class IRCbot
+        {
+            private readonly string _server;
+            private readonly int _port;
+            private readonly string _user;
+            private readonly string _nick;
+            private readonly string _channel;
+
+            private readonly int _maxRetries;
+
+            public IRCbot(string server, int port, string user, string nick, string channel, int maxRetries = 3)
+            {
+                _server = server;
+                _port = port;
+                _user = user;
+                _nick = nick;
+                _channel = channel;
+                _maxRetries = maxRetries;
+            }
+            public void Start()
+            {
+                var retry = false;
+                var retryCount = 0;
+                do
+                {
+                    try
+                    {
+                        using (var irc = new TcpClient(_server, _port))
+                        using (var stream = irc.GetStream())
+                        using (var reader = new StreamReader(stream))
+                        using (var writer = new StreamWriter(stream))
+                        {
+                            writer.WriteLine("NICK " + _nick);
+                            writer.Flush();
+                            writer.WriteLine(_user);
+                            writer.Flush();
+
+                            while (true)
+                            {
+                                string inputLine;
+                                while ((inputLine = reader.ReadLine()) != null)
+                                {
+                                    Console.WriteLine("<- " + inputLine);
+
+                                    // split the lines sent from the server by spaces
+                                    string[] splitInput = inputLine.Split(new Char[] { ' ' });
+
+                                    if (splitInput[0] == "PING")
+                                    {
+                                        string PongReply = splitInput[1];
+                                        //Console.WriteLine("->PONG " + PongReply);
+                                        writer.WriteLine("PONG " + PongReply);
+                                        writer.Flush();
+                                        //continue;
+                                    }
+                                    else if (splitInput[1] == "001")
+                                    {
+                                        writer.WriteLine("JOIN " + _channel);
+                                        writer.Flush();
+                                    }
+
+                                    else if (splitInput[1] == "PRIVMSG")
+                                    {
+                                        string str;
+
+                                        switch (splitInput[3])
+                                        {
+                                            case ":!update":
+                                                if (getSnookerInfo.snooker_update() != null)
+                                                {
+                                                    writer.WriteLine("PRIVMSG " + _channel + " :Cache refreshed.");
+                                                    writer.Flush();
+                                                }
+                                                break;
+                                            case ":!upcoming":
+                                                var tournaments = getSnookerInfo.snooker_upcoming();
+
+                                                foreach (var tournament in tournaments)
+                                                {
+                                                    writer.WriteLine("PRIVMSG " + _channel + " :" + tournament);
+                                                }
+                                                writer.Flush();
+                                                break;
+                                            case ":!next":
+                                                var nextT = getSnookerInfo.snooker_next();
+                                                writer.WriteLine("PRIVMSG " + _channel + " :" + nextT[1]);
+                                                writer.Flush();
+                                                break;
+                                            case ":!cat":
+                                                str = getSnookerInfo.snooker_cat();
+                                                writer.WriteLine("PRIVMSG " + _channel + " :" + str);
+                                                writer.Flush();
+                                                break;
+                                            default:
+                                                break;
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e.ToString());
+                        Thread.Sleep(5000);
+                        retry = ++retryCount <= _maxRetries;
+                    }
+                } while (retry);
+            }
+        }
+    }
+}
+
---
+++ getSnookerInfo.cs
@@ -0,0 +1,126 @@
+
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Security;
+using System.Security.Cryptography.X509Certificates;
+using System.Transactions;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace SnookerBot
+{
+    public class getSnookerInfo
+    {
+        public static async Task<string> snooker_update() {
+            var data = Task.Run(() => GetDataFromAPI("http://api.snooker.org/?t=5&s=2022"));
+            System.Net.ServicePointManager.ServerCertificateValidationCallback = (senderX, certificate, chain, sslPolicyErrors) => { return true; };
+            data.Wait();
+
+            await File.WriteAllTextAsync("./snooker_schedule.txt", data.Result);
+            return "1";
+        }
+
+        public static List<string> snooker_upcoming() {
+            string snookerInfo = File.ReadAllText(@"./snooker_schedule.txt");
+
+            dynamic array = JsonConvert.DeserializeObject(snookerInfo);
+            List<string> tournaments = new List<string>();
+            var x = 0;
+
+            foreach (var item in array)
+            {
+                var endDate = DateTime.Parse(item.EndDate.ToString());
+                if (endDate > DateTime.Now)
+                {
+                    // var dates = ModDate(item.StartDate, item.EndDate);
+                    tournaments.Add(x + ": " + item.Name + " | " + item.StartDate + " / " + item.EndDate + " | Type: " + item.Type);
+                    x++;
+                }
+                if (x == 5) { break; }
+            }
+
+            return tournaments;
+        }
+        public static List<string> snooker_next()
+        {
+            string snookerInfo = File.ReadAllText(@"./snooker_schedule.txt");
+
+            dynamic array = JsonConvert.DeserializeObject(snookerInfo);
+            List<string> tournaments = new List<string>();
+
+            string tCurrent = "None";
+            string tNext = "None";
+
+            foreach (var tournament in array)
+            {
+                string tName = tournament.Name;
+                string tType = tournament.Type;
+                string tStartDate = tournament.StartDate;
+                string tEndDate = tournament.EndDate;
+
+                // only select ranking tournaments and invitationals in the list
+                string[] invitationals = { "Masters", "Shanghai Masters", "Champion of Champions", "Paul Hunter Classic" };
+                if (tType == "Ranking" || Array.Find(invitationals, element => element == tName) != null)
+                {
+                    var StartT = DateTime.Parse(tStartDate);
+                    var EndT = DateTime.Parse(tEndDate);
+
+
+                    // Current Tournament
+                    if (StartT <= DateTime.Now && EndT.AddSeconds(76400) > DateTime.Now)
+                    {
+                        tCurrent = tName + " " + ModDate(tStartDate, tEndDate);
+                    }
+
+                    // Next Tournament
+                    if (StartT > DateTime.Now)
+                    {
+                        tNext = tName + " " + ModDate(tStartDate, tEndDate);
+                        break;
+                    }
+                }
+            }
+            List<string> arraytNext = new List<string>();
+            arraytNext.Add(tCurrent);
+            arraytNext.Add(tNext);
+            return arraytNext;
+        }
+        public static string snooker_cat()
+        {
+            var data = Task.Run(() => GetDataFromAPI("https://api.thecatapi.com/v1/images/search"));
+            System.Net.ServicePointManager.ServerCertificateValidationCallback = (senderX, certificate, chain, sslPolicyErrors) => { return true; };
+            data.Wait();
+
+            var jsonResult = data.Result.TrimStart(new char[] { '[' }).TrimEnd(new char[] { ']' });
+            JObject j = JObject.Parse(jsonResult);
+            string catURL = j["url"].ToString();
+
+            return catURL;
+        }
+
+        public static string ModDate(string StartDate, string EndDate)
+        {
+            var StartT = DateTime.Parse(StartDate);
+            var EndT = DateTime.Parse(EndDate);
+
+            if (StartT.Month == EndT.Month)
+            {
+                var returnDates = StartT.Day + "-" + EndT.Day + " " + EndT.ToString("MMM");
+                return returnDates;
+            }
+            else
+            {
+                var returnDates = StartT.ToString("d MMM") + " - " + EndT.ToString("d MMM");
+                return returnDates;
+            }
+        }
+
+        static async Task<string> GetDataFromAPI(string url)
+        {
+            var client = new HttpClient();
+            HttpResponseMessage result = await client.GetAsync(url);
+            var response = await result.Content.ReadAsStringAsync();
+
+            return response;
+        }
+    }
+}
